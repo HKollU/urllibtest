6b146fcf7fec05f18b5e8538639ca32e412ec14b
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-07 12:22:34 -0500

    Update getHTML.py

--- getHTML.py
+++ getHTML.py
@@ -3,7 +3,12 @@ theURL = 'https://www.youtube.com'
 webUrl = urllib.request.urlopen(theURL)
 print('Attempting to open' + theURL)
 if webUrl.getcode() != 200:
-        print('Opening' + theURL + 'Failed')
-        quit()
+	print('Opening' + theURL + 'Failed')
+	quit()
 data = webUrl.read()
+data=data.decode('utf-8')
 print(data)
+data = data.replace('10px','100px')
+
+f = open('output.html','wt',encoding='utf-8')
+f.write(data)
